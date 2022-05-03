30eb6f87246cb54b2f9d0ad5cb859332c8e7d847
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-05-03 08:07:20 -0300

    :tada: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Widget } from './components/Widget';
+import './global.css';
+
+type ButtonProps = {
+  text: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className='bg-violet-500 p-2 rounded'>{props.text ?? 'Default'}</button>
+}
+
+function App() {
+  return (
+    <div className='flex gap-2'>
+      <Widget />
+    </div>
+  )
+}
+
+export default App
