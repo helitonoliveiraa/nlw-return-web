92516de64dce692d8de60e50b8a72ba0af31db7b
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-05-06 08:13:05 -0300

    :sparkles: feat: create widget componente

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,13 +1,16 @@
 import { Popover } from '@headlessui/react'
 import { ChatTeardropDots } from 'phosphor-react';
+import { WidgetForm } from './WidgetForm';
 
 export function Widget() {
   return (
-    <Popover className='absolute bottom-10 right-10'>
-      <Popover.Panel>Hello nlw</Popover.Panel>
+    <Popover className='absolute bottom-4 right-4 md:bottom-10 md:right-10 flex flex-col items-end'>
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
 
       <Popover.Button 
-        className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'
+        className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group shadow-bg-brand-500'
       >
         <ChatTeardropDots className='w-6 h-6' />
 
